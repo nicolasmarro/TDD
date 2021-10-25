f6be961b0f7710d08797786c5804f2a25021731d
nicolas.marro <nicolas.marro@he-arc.ch>
2021-10-25 11:23:57 +0200

    Implementation exercice

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,11 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
     private int number;
 
     public FizzBuzz() {
     }
 
-    public void fizzBuzz(int number) {
+    public String fizzBuzz(int nb) throws IllegalArgumentException {
 
-        
+        if (String.valueOf(nb).contains("66") || (isMultipleOf5(nb) && isMultipleOf7(nb))) {
+            return "FizzBuzz";
+        }
+        else if (isMultipleOf5(nb)) {
+            return "Fizz";
+        }
+        else if (isMultipleOf7(nb)) {
+            return "Buzz";
+        }
+        else if (isNegative(nb)) {
+            throw new IllegalArgumentException("Nombre négatif");
+        }
+        else {
+            return String.valueOf(nb);
+        }
+    }
+
+    public List<String> generate(int start, int numberToGenerate) {
+        return IntStream.range(start, start + numberToGenerate)
+                .mapToObj(nb -> fizzBuzz(nb))
+                .collect(Collectors.toUnmodifiableList());
+
+        /*autre possibilité
+        List<String> result = new ArrayList<>();
+        for(int i = start; i<start+numberToGenerate; i++) {
+            result.add(fizzBuzz(i));
+        }
+        return result;*/
+    }
+
+    private boolean isMultipleOf5(int nb) {
+        return nb % 5 == 0;
+    }
+
+    private boolean isMultipleOf7(int nb) {
+        return nb % 7 == 0;
+    }
+
+    private boolean isNegative(int nb) {
+        return nb < 0;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(35));
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class FizzBuzzTest {
 
     @Test
@@ -10,9 +12,37 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void fizzBuzz_Given2_ShouldBe2() {
-        String result = FizzBuzz.fizzBuzz(2);
-        Assertions.assertEquals("2", result);
+    void fizzBuzz_GivenMultiple5_ShouldBeFizz() {
+        String result = fb.fizzBuzz(25);
+        Assertions.assertEquals("Fizz", result);
     }
 
+    @Test
+    void fizzBuzz_GivenMultiple7_ShouldBeBuzz() {
+        String result = fb.fizzBuzz(21);
+        Assertions.assertEquals("Buzz", result);
+    }
+
+    @Test
+    void fizzBuzz_GivenMultiple5and7_ShouldBeFizzBuzz() {
+        String result = fb.fizzBuzz(35);
+        Assertions.assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void fizzBuzz_GivenNegativeNumber_ShouldBeThrowIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> fb.fizzBuzz(-1));
+    }
+
+    @Test
+    void fizzBuzz_GivenNumberContains66_ShouldBeFizzBuzz() {
+        String result = fb.fizzBuzz(5669);
+        Assertions.assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void generate_Given3And4_ShouldReturn34Fizz6() {
+        List<String> result = fb.generate(3, 4);
+        Assertions.assertEquals(List.of("3", "4", "Fizz", "6"), result);
+    }
 }
